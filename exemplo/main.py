e5f8c20f4f2b285faffcbcbba015bc7052e6ed1b
m-novello <matheusnovello21@outlook.com>
2025-10-29 16:47:55 -0300

    Excluída a pasta de teste da biblioteca

--- exemplo/main.py
+++
@@ -1,5 +0,0 @@
-# main.py
-from mathematics import pitagoras  # Importação limpa graças ao __init__.py
-
-resultado = pitagoras(3, 4)
-print(resultado)  # Output: 5.0
--- exemplo/mathematics/__init__.py
+++
@@ -1,6 +0,0 @@
-# __init__.py
-from .geometric import pitagoras  # Importa a função do módulo
-
-__all__ = [
-    'pitagoras'  # Lista o que fica disponível com "from mathematics import *"
-]
